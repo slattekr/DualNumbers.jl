5677b4431dcdf09540bcff6a1e5baf454fb76bce
Katherine Slattery <kslattery@perimeterinstitute.ca>
2023-01-16 14:52:21 -0500

    Implement scalar multiplication

--- src/DualNumbers.jl
+++ src/DualNumbers.jl
@@ -9,8 +9,17 @@ struct Dual{T}
 	dual::T
 end
 
-Base.:+(x::Dual, y::Dual) = Dual(x.real + y.real, x.dual + y.dual)
+#Identity and negation
+Base.:+(x::Dual) = x
+Base.:-(x::Dual) = Dual(-x.real, -x.dual)
 
+#Addition and subtraction operators
+Base.:+(x::Dual, y::Dual) = Dual(x.real + y.real, x.dual + y.dual)
 #Base.:-(x::Dual, y::Dual) = Dual(x.real - y.real, x.dual - y.dual)
-Base .:-(x::Dual, y::Dual) = x + -y
+Base.:-(x::Dual, y::Dual) = x + -y
+
+#scalar Multiplication
+Base.:*(a::Number, x::Dual) = Dual(a * x.real, a * x.dual)
+Base.:*(x::Dual, a::Number) = Dual(x.real * a, x.dual * a)
+
 end # module DualNumbers
