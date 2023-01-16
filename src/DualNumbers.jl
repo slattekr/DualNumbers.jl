bfee2887f727b39e7d95c13710830495203bb942
Katherine Slattery <kslattery@perimeterinstitute.ca>
2023-01-16 15:08:31 -0500

    'Added implementation for multiplication'

--- src/DualNumbers.jl
+++ src/DualNumbers.jl
@@ -22,4 +22,22 @@ Base.:-(x::Dual, y::Dual) = x + -y
 Base.:*(a::Number, x::Dual) = Dual(a * x.real, a * x.dual)
 Base.:*(x::Dual, a::Number) = Dual(x.real * a, x.dual * a)
 
+#multiplication
+# x = a + eps b
+# y = c + eps b
+# eps^2 = 0
+#x * y = a*c+eps * (a*d + c*b)
+Base.:*(x::Dual, y::Dual) = Dual(x.real * y.real, x.real * y.dual + x.dual *y.real)
+#Conjugate
+Base.conj(x::Dual) = Dual(x.real, -x.dual)
+
+#Inverse
+#	1/(a + eps b)
+#	= (a-eps b) / [(a+ eps b)*(a - eps b)]
+#	= (a - eps b)/ a^2
+#	= (1 /a^2) * (a - eps b)
+Base.inv(x::Dual) = conj(x) * (1/x.real^2)
+#division
+Base.:/(x::Dual, y::Dual) = x* inv(y)
+
 end # module DualNumbers
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,13 @@ y = Dual(2, 3)
 z = x+y
 @test z == Dual(3, 5)
 
+@test +x == x
+@test -x== Dual(-1, -2)
+
+@test x+y == Dual(3, 5)
+@test x-y == Dual(-1, -1)
+
+@test 4* x == Dual(4, 8)
+@test  y*5  == Dual(10, 15)
+
+@test 
